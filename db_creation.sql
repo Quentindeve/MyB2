216eb150c37d10559ce24821e02e9ba73998d547
Quentin <38379985+Quentindeve@users.noreply.github.com>
2023-10-05 22:07:35 +0200

    Delete db_creation.sql
    useless file

--- db_creation.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `products`
-(
- `image`       varchar(32) NOT NULL ,
- `name`        varchar(200) NOT NULL ,
- `price`       float NOT NULL ,
- `description` varchar(500) NOT NULL ,
-
-PRIMARY KEY (`image`)
-);
